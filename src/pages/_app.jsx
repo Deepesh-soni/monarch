b9ae59df6e87b6eb2474164e9fd08d1ceec3878d
Deepesh Soni <dsoni.ds21@gmail.com>
2025-03-30 23:30:36 +0530

    feat: refactor code

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,7 +16,7 @@ import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import Session from "supertokens-auth-react/recipe/session";
 
 import SuperTokensReact, { SuperTokensWrapper } from "supertokens-auth-react";
-import { frontendConfig } from "./../../config/frontendConfig";
+import { frontendConfig } from "../config/frontendConfig";
 
 if (typeof window !== "undefined") {
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
