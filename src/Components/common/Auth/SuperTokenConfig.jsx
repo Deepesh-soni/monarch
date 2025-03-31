1eda0f2db3a0f802816b3463bbddccac2812c2d6
UserX <x@tt.co>
2025-04-01 02:24:16 +0530

    Supertoken fix

--- src/Components/common/Auth/SuperTokenConfig.jsx
+++ src/Components/common/Auth/SuperTokenConfig.jsx
@@ -5,7 +5,7 @@ import Router from 'next/router'
 const appInfo = {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "StockApp",
-    apiDomain: "http://iosskwk0k8cgkcs8k0o80wc8.188.165.196.11.sslip.io",
+    apiDomain: "https://api-test-monarq.pamprazzi.in",
     websiteDomain: "http://localhost:3000",
     apiBasePath: "/auth",
     websiteBasePath: "/auth"
