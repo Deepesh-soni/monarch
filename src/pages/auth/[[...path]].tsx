daf2d70080b1d84e11f9a1bb5a8085f1f552b8de
UserX <x@tt.co>
2025-04-01 13:32:23 +0530

    Fix

--- src/pages/auth/[[...path]].tsx
+++ src/pages/auth/[[...path]].tsx
@@ -3,10 +3,10 @@ import dynamic from 'next/dynamic'
 import { EmailPasswordPreBuiltUI } from 'supertokens-auth-react/recipe/emailpassword/prebuiltui';
 import { redirectToAuth } from 'supertokens-auth-react'
 import { canHandleRoute, getRoutingComponent } from 'supertokens-auth-react/ui'
-import Navbar from '@common/Navbar';
 import styled from 'styled-components';
 import FlexBox from '@Components/common/UI/FlexBox';
 import { device } from '@Components/common/UI/Responsive';
+import NavBar from '@Components/common/Navbar';
 
 
 const Wrapper = styled.div`
@@ -49,7 +49,7 @@ export default function Auth() {
 
     return (
         <Wrapper>
-            <Navbar />
+            <NavBar />
             <Container>
                 <div style={{ marginTop: '40px', width: '100%', maxWidth: '480px' }}>
                     <SuperTokensComponentNoSSR />
