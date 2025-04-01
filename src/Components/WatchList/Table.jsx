7321d075daa9eff4d4d384c47104629507257b7d
Deepesh Soni <dsoni.ds21@gmail.com>
2025-04-01 16:28:43 +0530

    fix: watchlist card click

--- src/Components/WatchList/Table.jsx
+++ src/Components/WatchList/Table.jsx
@@ -87,14 +87,17 @@ const Table = () => {
     stocks: withDefault(ArrayParam, []),
   });
   const [loading, setLoading] = useState();
+  const [stocks, setStocks] = useState();
 
   const fetchWatchlists = async () => {
+    debugger;
     setLoading(true);
     try {
       const res = await client.post("/stock/by-fqns", {
         fqns: queryParams?.stocks,
       });
-      // setWatchlists(response.data || []);
+      setStocks(res.data || []);
+      debugger;
     } catch (error) {
       console.error("Failed to fetch watchlists", error);
     }
@@ -102,8 +105,8 @@ const Table = () => {
   };
 
   useEffect(() => {
-    fetchWatchlists();
-  }, []);
+    if (!!queryParams?.stocks?.length) fetchWatchlists();
+  }, [queryParams?.stocks]);
 
   return (
     <Wrapper>
@@ -125,18 +128,18 @@ const Table = () => {
               </tr>
             </thead>
             <tbody>
-              {data.map((row, index) => (
+              {stocks.map((stock, index) => (
                 <Tr key={index}>
-                  <Td>{row.name}</Td>
-                  <Td>{row.CMP}</Td>
-                  <Td>{row.PE}</Td>
-                  <Td>{row.marketCap}</Td>
-                  <Td>{row.dividend}</Td>
-                  <Td>{row.netProfit}</Td>
-                  <Td>{row.profitVar}</Td>
-                  <Td>{row.sales}</Td>
-                  <Td>{row.salesVar}</Td>
-                  <Td>{row.ROCE}</Td>
+                  <Td>{stock?.companyShortName}</Td>
+                  <Td>{stock?.CMP}</Td>
+                  <Td>{stock?.PE}</Td>
+                  <Td>{stock?.marketCap}</Td>
+                  <Td>{stock?.dividend}</Td>
+                  <Td>{stock?.netProfit}</Td>
+                  <Td>{stock?.profitVar}</Td>
+                  <Td>{stock?.sales}</Td>
+                  <Td>{stock?.salesVar}</Td>
+                  <Td>{stock?.ROCE}</Td>
                 </Tr>
               ))}
             </tbody>
